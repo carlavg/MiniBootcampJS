04bd7081816100ae63b338b58c4d66ea26ed208d
carlavg <70992549+carlavg@users.noreply.github.com>
2022-04-29 12:32:27 -0300

    Subido el ejercicio numero 1.

---
+++ UNIDAD1/ejercicio01.js
@@ -0,0 +1,5 @@
+alert("Hola! Ve a Inspeccionar -> Console -> y verifica mis datos cargados"); 
+console.log("a) El día de mi cumpleaños es:", 9);
+console.log("b) El mes de mi cumpleaños es:", 5); 
+console.log("c) El año de mi nacimiento es:", 1992);
+console.log("d) Mi número favorito es:", 18); 
