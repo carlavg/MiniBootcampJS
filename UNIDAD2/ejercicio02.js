4c3ebf7f0bc0ee46c3a1b0fbff0a4b6536eff30f
carlavg <70992549+carlavg@users.noreply.github.com>
2022-04-29 12:45:06 -0300

    Subido el ejercicio 02

---
+++ UNIDAD2/ejercicio02.js
@@ -0,0 +1,24 @@
+var cantidadDeVentanasEnMiCasa; 
+cantidadDeVentanasEnMiCasa = 6; 
+console.log(cantidadDeVentanasEnMiCasa);
+
+var soyHumano;
+soyHumano = true; 
+console.log(soyHumano); 
+
+var miGustoDePizzaPreferido; 
+miGustoDePizzaPreferido = "Pizza a la Fugazzeta"; 
+console.log(miGustoDePizzaPreferido); 
+
+var mutable; 
+mutable = false; 
+console.log(mutable); 
+
+mutable = "Hola"; 
+console.log(mutable); 
+
+mutable = 0; 
+console.log(mutable); 
+
+mutable = "Minions"; 
+console.log(mutable);
